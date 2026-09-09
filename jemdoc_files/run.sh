bc844482dd5473b52acac09872dc7a7120be065a
juntaowang <223010102@link.cuhk.edu.cn>
2026-09-09 10:55:14 +0800

    update lecture notes

--- jemdoc_files/run.sh
+++ jemdoc_files/run.sh
@@ -2,4 +2,5 @@
 
 set -e
 python ../jemdoc -c mysite.conf -o ../www/ *.jemdoc
+cp ../AIE6001_S1_Lecture0.pdf ../AIE6001_S1_Lecture1.pdf ../lecture1_vector_space_explorer.html ../www/
 echo "compilation done!"
--- update.sh
+++ update.sh
@@ -6,5 +6,7 @@ set -e
 git pull origin main
 cd jemdoc_files
 python ../jemdoc -c mysite.conf -o ../www/ *.jemdoc
+cd ..
+cp AIE6001_S1_Lecture0.pdf AIE6001_S1_Lecture1.pdf lecture1_vector_space_explorer.html www/
 
 echo "site regenerated"
